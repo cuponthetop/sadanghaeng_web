a2cc2028a2081efc09125f123007b4fe493e6a2d
Donghyun Go <godong9@gmail.com>
2016-02-20 10:55:58 +0900

    Update

---
+++ public/js/join-page/app.js
@@ -0,0 +1,18 @@
+(function (window) {
+  'use strict';
+
+  /**
+   * Set up join page
+   *
+   * @param {string}
+   */
+
+  function bind() {
+
+  }
+
+  $(document).ready(function() {
+    bind();
+  });
+
+})(window);
--- public/js/post-page/view.js
+++ public/js/post-page/view.js
@@ -40,6 +40,10 @@
     viewCommands[viewCmd]();
   };
 
+  View.prototype.bind = function (event, handler) {
+
+  };
+
   // Export to window
   window.app = window.app || {};
   window.app.View = View;
