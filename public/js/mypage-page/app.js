7a313778f340dbbc00199a5e109040887afac089
Donghyun Go <godong9@gmail.com>
2016-02-23 13:05:08 +0900

    Update mypage

---
+++ public/js/mypage-page/app.js
@@ -0,0 +1,18 @@
+(function (window) {
+  'use strict';
+
+  /**
+   * Set up login page
+   *
+   * @param {string}
+   */
+
+  function bind() {
+
+  }
+
+  $(document).ready(function() {
+    bind();
+  });
+
+})(window);
