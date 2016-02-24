60c20769e0606be573e451941580fbeb2f536fb9
garin <gairn0112@naver.com>
2016-02-24 22:08:18 +0900

    mypage

--- public/js/mypage-page/app.js
+++ public/js/mypage-page/app.js
@@ -6,13 +6,28 @@
    *
    * @param {string}
    */
+  function save() {
+    var userNickname = $('#user_input').val();
+
+    if(userNickname==""){
+      alert("변경할 닉네임을 입력해주세요.");
+      $('#user_input').focus();
+      return false;
+    }else {
+      alert("저장되었습니다.");
+      var params = {'userNickname': userNickname}
+    }
+  }
 
   function bind() {
-
+    $('#editBtn').click(function() {
+      save();
+    });
   }
 
   $(document).ready(function() {
     bind();
   });
+
 
 })(window);
