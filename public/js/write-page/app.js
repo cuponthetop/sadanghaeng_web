71a696a9d2eb759320c3a0eba8fa9aba407250b2
HwasooSung <hwasu92@naver.com>
2016-02-24 22:11:35 +0900

    add validate() in click func and change regex

--- public/js/write-page/app.js
+++ public/js/write-page/app.js
@@ -13,7 +13,7 @@
    var text = $('#post_content').val();
    
    var status = true;
-   if (!title || title.test(regExCheckEmptyText)) {
+   if (!title || regExCheckEmptyText.test(title)) {
      alert("글 제목을 제대로 입력해주세요!");
      status = false;
    }
@@ -25,17 +25,19 @@
   }
 
   function bind() {
-    $('.post-btn').click(function() {
-      var post = {
-        title: $('#post_title').val(),
-        text: $('#post_content').val()
-      };
-      HttpUtil.post('http://localhost:5001/api/v1/posts', post, function(err, result) {
-        if (err) {
-          return;
-        }
-        console.log(result);
-      });
+    $('.post-btn').click(function () {
+      if (validate()) {
+        var post = {
+          title: $('#post_title').val(),
+          text: $('#post_content').val()
+        };
+        HttpUtil.post('http://localhost:5001/api/v1/posts', post, function (err, result) {
+          if (err) {
+            return;
+          }
+          console.log(result);
+        });
+      }
     });
   }
 
